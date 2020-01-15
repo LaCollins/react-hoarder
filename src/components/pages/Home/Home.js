d183fcd92f5996adf89533e1674128f80aa3e67b
Laura Collins <laura.e.collins1@gmail.com>
2020-01-14 21:23:09 -0600

    done with styling home page

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -6,7 +6,10 @@ import './Home.scss';
 class Home extends React.Component {
   render() {
     return (
+      <div className="Home">
       <h1>HOME</h1>
+        <img src="https://1.bp.blogspot.com/-orq93c1aGrE/XVYa0uvWTjI/AAAAAAAATKY/TET_V74Tk-MynNOr0FscWzZMozR8vrgzwCLcBGAs/s1600/messy%2Broom%2B2.JPG" alt="junk pile" />
+      </div>
     );
   }
 }
